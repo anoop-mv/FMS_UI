b41c4dab0fd451f268f9d7db13fc3e35edd5d105
MV <724429@cognizant.com>
2019-11-24 20:31:29 +0530

    Eventlist

--- src/app/components/eventlist/eventlist.component.ts
+++ src/app/components/eventlist/eventlist.component.ts
@@ -13,21 +13,25 @@ export class EventlistComponent implements OnInit {
   ngOnInit() {
     this.datalist= [
       {
+        id:1,
         name: "view",
         inventory: 5,
         unit_price: 45.99
       },
       {
+        id:1,
         name: "view",
         inventory: 10,
         unit_price: 123.75
       },
       {
+        id:1,
         name: "view",
         inventory: 2,
         unit_price: 399.50
       }
     ];
   }
+  
 
 }
